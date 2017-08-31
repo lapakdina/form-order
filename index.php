0f7496b756251489c9a0ff403f2b6bab606ac55a
lapak dina <31301687+lapakdina@users.noreply.github.com>
2017-08-31 23:10:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 <body>
  
 	<h1>Contact Form Email</h1>
-	<p>Demo ini dibuat dan di upload oleh <a href="http://tutorialweb.net/" target="_blank">tutorialweb.net</a></p>
-	
+		
 	<form action="kirim.php" method="post">
 		<p><input type="text" name="nama" placeholder="Nama anda" size="30" required /></p>
 		<p><input type="email" name="email" placeholder="Email valid" size="30" required /></p>
